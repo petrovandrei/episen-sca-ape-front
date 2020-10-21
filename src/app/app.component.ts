2880947eb1b819c62b11e015644d7e665d5a0af6
Andrei Petrov <andrei.petrov@etu.u-pec.fr>
2020-10-22 00:07:39 +0200

    push du code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ApiService} from './services/api-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'episen-sca-ape-front';
+
+  order: string;
+
+  constructor(private router: Router, private route: ActivatedRoute, private  apiService: ApiService) {
+
+  }
+
+  sendOrder(): void {
+    this.apiService.sendOrder(this.order);
+  }
 }
